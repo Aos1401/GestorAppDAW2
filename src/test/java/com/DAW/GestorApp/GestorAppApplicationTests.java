4165509a1a2741efaa61e721fbcd9204598fd882
sabsdiass <sabrinadiiascadastros2408@gmail.com>
2025-10-22 10:49:22 +0200

    Ready to upload the main project.

---
+++ src/test/java/com/DAW/GestorApp/GestorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DAW.GestorApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
